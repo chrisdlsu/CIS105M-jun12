2217d64c97d41cc31c15f23d9047c40145c3693c
chrisdlsu <chris_perez@dlsu.edu.ph>
2023-06-12 18:11:18 +0800

    updates

--- Main.java
+++ Main.java
@@ -3,8 +3,16 @@ public class Main {
         System.out.println("hello world!");
         mySquare bonggo = new mySquare();
 
+        int nBasket;
+        nBasket = bonggo.addTwo(256);
+        System.out.println(nBasket);
+
         for (int i = 1; i < 11; i++) {
             bonggo.sayHello();
+            System.out.println(bonggo.addTwo(i));
         }
+
+        System.out.println(bonggo.isASquare(1));
+        System.out.println(bonggo.sayWeather(1));
     }
 }
--- mySquare.java
+++ mySquare.java
@@ -1,5 +1,27 @@
 public class mySquare {
 
+
+    // method with a return value
+    public int addTwo(int i) {
+        return i + 2;
+    }
+
+    public boolean isASquare(int x) {
+        if (x == 1) {
+            return true;
+        }
+        return false;
+    }
+
+    public String sayWeather(int y) {
+        if (y > 5) {
+            return "GreaterFive";
+        }
+        return "LessFive";
+    }
+
+
+
     //print hello yeng
     public void sayHello() {
         System.out.println("Hello Yeng!");
